a2b84605912d8cab0df0695b4e7b2921c6cb001a
PaulJohnson19 <135523533+PaulJohnson19@users.noreply.github.com>
2023-06-04 14:21:44 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+var myAge = '38'
+//myAge is current age
+
+var earlyYears = 2
+//first two years of dog life have different value
+
+var earlyYears = earlyYears *= 10.5
+//aging equation of first two years of dog's life
+
+var laterYears = myAge - 2
+
+var laterYears = laterYears *= 4
+
+var myAgeInDogYears = laterYears + earlyYears
+//equation that converts my age to dog years 
+
+var myName = 'PAUL'.toLowerCase()
+//name goes presto to little guys
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`)
